90edb4422a779dd0607cb088e453b38c8642f7fd
vanglevan <itlvvang.0503@gmail.com>
2018-09-25 19:53:52 +0700

    update config for production environment

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  mongoURI: process.env.MONGO_URI || 'mongodb://admin:ChimSe2021@ds261332.mlab.com:61332/mydevconnector',
+  secretOrKey: process.env.SECRET_OR_KEY || 'secretkey'
 };
